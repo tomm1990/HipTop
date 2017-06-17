71572688a10cd610c821e362b1652e6619dbbe41
TomGoldberg <tomg3210@gmail.com>
2017-06-17 21:01:05 +0300

    9nd commit : - database.js fix

--- src/service/database.js
+++ src/service/database.js
@@ -1,20 +1,20 @@
-
+//------------Connect to mongodbon mLab via Mongoose---------------//
 const consts = require('./consts/constring'),
-    mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+    mongoose = require('mongoose'),
+    //The server option auto_reconnect is defaulted to true
+     options = {
+      server: {
+        auto_reconnect:true,
+        socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }
+      },
+      replset: {
+        socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }
+      }
+    };
 
-//The server option auto_reconnect is defaulted to true
-var options = {
-  server: {
-    auto_reconnect:true,
-    socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }
-  },
-  replset: {
-    socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 }
-  }
-};
-
+// mongoose.Promise = global.Promise;
 mongoose.connect(consts.MLAB_KEY, options);
+
 const conn = mongoose.connection;//get default connection
 // Event handlers for Mongoose
 conn.on('error', function (err) {
